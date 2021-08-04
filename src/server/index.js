788d4f7cb95426301d7e18eee6435f7da3f73500
CaptSzat <49853752+CaptSzat@users.noreply.github.com>
2021-08-04 18:00:03 +1000

    i

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,15 @@
-var app = require('http').createServer()
-var io = module.exports.io = require('socket.io')(app)
+const express = require('express')
+const app = exress()
+var server = require('http').createServer(app)
+var io = module.exports.io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3231
 
 const SocketManager = require('./SocketManager')
 
+app.use(express.static(_dirname + '/../../build'))
 io.on('connection', SocketManager)
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
 	console.log("Connected to port:" + PORT);
 })
